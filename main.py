13ceafceffcee7aa16277bb47f8035bbdd9da42b
ecz5032 <ecz5032@psu.edu>
2020-08-29 00:26:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+tempC = input("Enter temperature in celsius: ") 
+tempC = float(tempC)
+tempF = (tempC *9/5) + 32
+print(str(tempC) + "° in Celsius is equivalent to " + str(tempF) + "° in Fahrenheit")
